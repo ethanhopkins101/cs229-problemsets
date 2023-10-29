2493a7041c8731583cb2dea94ec151e7936d0fb2
ethan <ethan.hopkins101@gmail.com>
2023-10-29 22:19:49 +0100

    Guassian discriminant analysis from scratch

--- pb_set1/gda/p01e_gda.py
+++ pb_set1/gda/p01e_gda.py
@@ -1,68 +1,31 @@
-from statistics import mean
 import numpy as np
-import util
+from math import pi
+from math import sqrt
+class Gda():
+    def __init__(self):
+        self.feta=self.mu0=self.mu1=self.sigma=0
 
-from linear_model import LinearModel
-
-
-def main(train_path, eval_path, pred_path):
-    """Problem 1(e): Gaussian discriminant analysis (GDA)
-
-    Args:
-        train_path: Path to CSV file containing dataset for training.
-        eval_path: Path to CSV file containing dataset for evaluation.
-        pred_path: Path to save predictions.
-    """
-    # Load dataset
-    x_train, y_train = util.load_dataset(train_path, add_intercept=False)
-
-    # *** START CODE HERE ***
-    # *** END CODE HERE ***
-
-
-class GDA(LinearModel):
-    """Gaussian Discriminant Analysis.
-
-    Example usage:
-        > clf = GDA()
-        > clf.fit(x_train, y_train)
-        > clf.predict(x_eval)
-    """
 
     def fit(self, x, y):
-        m=y.shape[0]
-        feta=np.sum(y)/y.shape[0]
-        alfa=np.argwhere(y==0)
-        beta=np.argwhere(y==1)
-        mu0=x[alfa]/len(alfa)
-        mu1=x[beta]/len(beta)
-        mean_vector=np.array([[1 for _ in range(y.shape[0])]])
-        for i in range(y.shape[0]):
-            if y[i]==1:
-                mean_vector[i]=mu1
-            else:
-                mean_vector[i]=mu0
-        sigma=(1/m)*np.dot(x-mean_vector,(x-mean_vector).T)
-        """Fit a GDA model to training set given by x and y.
-
-        Args:
-            x: Training example inputs. Shape (m, n).
-            y: Training example labels. Shape (m,).
-
-        Returns:
-            theta: GDA model parameters.
-        """
-        # *** START CODE HERE ***
-        # *** END CODE HERE ***
+        x0=x[y[:,0]==0]
+        x1=x[y[:,0]==1]
+        m=x.shape[0]
+        self.feta=np.mean(y)
+        self.mu0=np.mean(x0,axis=0)
+        self.mu1=np.mean(x1,axis=0)
+        ## not sure of the next syntax
+        mu_vec=np.where(y==1,self.mu1,self.mu0)
+        self.sigma=(1/m)*np.dot((x-mu_vec).T,x-mu_vec)
 
     def predict(self, x):
-        """Make a prediction given new inputs x.
-
-        Args:
-            x: Inputs of shape (m, n).
-
-        Returns:
-            Outputs of shape (m,).
-        """
-        # *** START CODE HERE ***
-        # *** END CODE HERE
+         #           p(x/y=1)p(y=1)                      p(x/y=0)p(y)
+    # p(y=1|x)=       ____________            p(y=0|x) =  __________
+    #                     p(x)                                p(x)
+        px_y0=1/(((2*pi)**(x.shape[1]/2))*sqrt(np.linalg.det(self.sigma)))*np.exp(-(np.dot(np.dot(x-self.mu0,np.linalg.inv(self.sigma)),(x-self.mu0).T)/2))
+        px_y1=1/(((2*pi)**(x.shape[1]/2))*sqrt(np.linalg.det(self.sigma)))*np.exp(-(np.dot(np.dot(x-self.mu1,np.linalg.inv(self.sigma)),(x-self.mu1).T)/2))
+        py1=self.feta
+        py0=1-self.feta
+        px=px_y0*py0+px_y1*py1
+        py0_x=px_y0*py0/px
+        py1_x=px_y1*py1/px
+        return max(py1_x,py0_x)
