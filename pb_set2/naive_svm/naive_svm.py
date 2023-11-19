f70e06c017efb1dd2f707bab54846c8e9ed10b9e
ethan <ethan.hopkins101@gmail.com>
2023-11-19 20:58:53 +0100

    naive bayes and perceptron from scratch

---
+++ pb_set2/naive_svm/naive_svm.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+class BernoulliNB:
+    def __init__(self):
+        self.pxy1 = self.pxy0 = self.py0 = self.py1 = 0
+
+    def fit(self, x, y):
+        x_spam = x[y[:, 0] == 1]
+        x_ham = x[y[:, 0] == 0]
+        self.pxy1 = self.pxy0 = [0 for _ in range(x.shape[1])]
+
+        for i in range(x.shape[1]):
+            self.pxy1[i] = np.sum(x_spam[:, i]) / x_spam.shape[0]
+            self.pxy0[i] = np.sum(x_ham[:, i]) / x_ham.shape[0]
+        self.py1 = x_spam.shape[0] / x.shape[0]
+        self.py0 = 1 - self.py1
+
+    def predict(self, x):
+        temp_yes = []
+        temp_no = []
+        for i, j in enumerate(x):
+            if j == 1:
+                temp_yes.append(self.pxy1[i])
+                temp_no.append(self.pxy0[i])
+        yes = self.py1 * np.prod(temp_yes)
+        no = self.py0 * np.prod(temp_no)
+        return 1 if yes > no else 0
